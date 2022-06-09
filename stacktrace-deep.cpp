b98e1c17d02e1ce8e3e023c79a46c1fdf6d3804b
nicole mazzuca <mazzucan@outlook.com>
2022-06-09 11:51:48 -0700

    add stacktrace-deep test

---
+++ stacktrace-deep.cpp
@@ -0,0 +1,34 @@
+#include <stacktrace>
+#include <iostream>
+
+void print_stacktrace()
+{
+    auto s = std::stacktrace::current(10);
+    if (s.empty())
+    {
+        std::cout << "failed to initialize\n";
+    }
+    else
+    {
+        std::cout << *s.rbegin() << '\n';
+    }
+}
+
+#pragma optimize("", off) // inhibit TCO
+void stackin(unsigned current_frame)
+{
+    if (current_frame == 0x10000)
+    {
+        print_stacktrace();
+    }
+    else
+    {
+        stackin(current_frame + 1);
+    }
+}
+#pragma optimize("", on)
+
+int main()
+{
+    stackin(0);
+}

a926c646ae030ec25b7497b7d71253fc4546ce30
Nicole Mazzuca <mazzucan@outlook.com>
2022-09-15 13:50:17 -0700

    add support for changed and compiler number

--- run-tests.ps1
+++ run-tests.ps1
@@ -6,6 +6,10 @@ Param (
   [String]$Flavor,
   [Parameter(Mandatory=$False)]
   [String]$BuildDir,
+  [Parameter(Mandatory=$False)]
+  [Int]$Compiler = -1,
+  [Parameter(Mandatory=$False, ParameterSetName='STL')]
+  [Switch]$Changed,
   [Parameter(Mandatory=$False, ParameterSetName='STL')]
   [Switch]$STL,
   [Parameter(Mandatory=$True, ParameterSetName='LLVM')]
@@ -31,9 +35,47 @@ if (-not $?) {
   throw
 }
 
+if ($Changed) {
+  $expectedMainBranches = @('main', 'master', 'trunk', 'dev', 'devel')
+  $mainBranch = ([String[]]($expectedMainBranches | % {
+    if (-not [String]::IsNullOrEmpty((git branch --list $_))) {
+      $_
+    }
+  }))[0]
+  if ($null -eq $mainBranch) {
+    throw "Could not find main branch"
+  }
+
+  [String[]]$changedTestFiles = git diff --name-only "$mainBranch...HEAD" "tests/$testSet/tests"
+
+  $changedTests = $changedTestFiles | % {
+    Split-Path -Parent $_
+  } | Sort-Object -Unique | % {
+    if ($_ -match '^tests\\std\\tests\\([^\\].*)$') {
+      $Matches[1]
+    }
+  }
+
+  if (-not [String]::IsNullOrEmpty($Filter)) {
+    $changedTests = $changedTests + @($Filter)
+  }
+  $joiner = ')|(' # has a `)`, so we can't just put it in the string
+  $Filter = "($($changedTests -join $joiner))"
+}
+
+if ($Compiler -ge 0) {
+  $compilerFilter = ".*:$($Compiler.ToString('D2'))$"
+  if (-not [String]::IsNullOrEmpty($Filter)) {
+    $Filter = "($Filter)$compilerFilter"
+  } else {
+    $Filter = $compilerFilter
+  }
+}
+
 $filterArg = @()
 if (-not [String]::IsNullOrEmpty($Filter)) {
   $filterArg = @('--filter', $Filter)
 }
 
+echo "python ./out/$Flavor/tests/utils/stl-lit/stl-lit.py -vv ./tests/$testSet $($filterArg -join ' ')"
 python ./out/$Flavor/tests/utils/stl-lit/stl-lit.py -vv ./tests/$testSet @filterArg
